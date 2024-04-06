8f9c5304a56dccc9c6a396147096712530c46d35
Hami Ibriyamov <hamop3108@gmail.com>
2024-04-06 19:35:27 +0300

    Create MyAdoptionsViewModel.cs

---
+++ PetAdoptionMobileApplication/ViewModels/MyAdoptionsViewModel.cs
@@ -0,0 +1,49 @@
+using PetAdoptionMobileApplication.Services;
+
+namespace PetAdoptionMobileApplication.ViewModels
+{
+    public partial class MyAdoptionsViewModel : BaseViewModel
+    {
+        private readonly AuthService authService;
+        private readonly IUserAPI userAPI;
+
+        public MyAdoptionsViewModel(AuthService authService, IUserAPI userAPI)
+        {
+            this.authService = authService;
+            this.userAPI = userAPI;
+        }
+
+        [ObservableProperty]
+        private IEnumerable<PetListDTO> userFavs = Enumerable.Empty<PetListDTO>();
+
+        public async Task InitializeAsync()
+        {
+            // Check if user is not logged in
+            if (!this.authService.IsLoggedIn)
+            {
+                await ShowToastAsync("You need to be logged in!");
+                return;
+            }
+
+            IsBusy = true;
+            try
+            {
+                await Task.Delay(100);
+                var APIResponse = await this.userAPI.GetUserAdoptionsAsync();
+                if (APIResponse.IsSuccess)
+                {
+                    UserFavs = APIResponse.Data;
+                }
+                else
+                {
+                    await ShowAlertAsync("An error occured while fetching this data!", APIResponse.Message, "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await ShowAlertAsync("Something went wrong!", ex.Message, "OK");
+                IsBusy = false;
+            }
+        }
+    }
+}
